bd77b25a1c68329d0dd3c64b6eb1b666b3501639
Surepalli-Ashokkumar <138860355+Surepalli-Ashokkumar@users.noreply.github.com>
2024-09-11 21:34:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Movie from './Project/Movie';
+
+
+
+function App() {
+  return (
+    <div>
+      <Movie />
+
+    </div>
+  );
+}
+
+export default App;
+ 
---
+++ Demo.js
@@ -0,0 +1,9 @@
+import React from 'react';  
+function Demo() { 
+    return ( 
+    <div> <header className="App-header"> 
+    <h1>Hello, World!</h1> </header> </div>
+     ); 
+    }
+     export default Demo;
+		
